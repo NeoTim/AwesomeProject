4b81499e8dd31ac7d870a756109f6a7b981c4019
lixd <875697126@qq.com>
2016-07-27 08:46:10 +0800

    add homereducer rootreducer

---
+++ app/actions/actionTypes.js
@@ -0,0 +1,16 @@
+//main page actions
+export const FETCH_HOME_LIST = 'FETCH_HOME_LIST';
+export const RECEIVE_HOME_LIST = 'RECEIVE_HOME_LIST';
+
+// detail page actions
+// export const FETCH_DETIAL_DATE = 'FETCH_DETIAL_DATE';
+// export const RECEIVE_DETIAL_DATE = 'RECEIVE_DETIAL_DATE';
+
+//class page actions
+export const FETCH_CLASS_LIST = 'FETCH_CLASS_LIST';
+export const RECEIVE_CLASS_LIST = 'RECEIVE_CLASS_LIST';
+
+//classd detail page actions
+export const FETCH_CLASSDITAL_LIST = 'FETCH_CLASSDITAL_LIST';
+export const RECEIVE_CLASSDITAL_LIST = 'RECEIVE_CLASSDITAL_LIST';
+export const RESET_CLASSDITAL_STATE = 'RESET_CLASSDITAL_STATE'; 
