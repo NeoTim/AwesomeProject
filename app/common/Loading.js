522add5ec74713eab70fb9a5e44642da1170b2e7
lixd <875697126@qq.com>
2016-07-29 08:07:53 +0800

    update

--- app/common/Loading.js
+++ app/common/Loading.js
@@ -1,45 +1,39 @@
-/**
- * Created by lixd on 16/7/28.
- */
 import React from 'react';
 import {
     StyleSheet,
     View,
     Text,
-    ActivityIndicator,
-    } from 'react-native';
+    ActivityIndicator
+} from 'react-native';
 
-import Common from '../common/common';
+import Common from './common.js';
 
 export default class Loading extends React.Component {
-    render() {
+    render(){
         return (
             <View style={styles.loading}>
-                <ActivityIndicator color="white"/>
-                <Text style={styles.loadingTitle}>加载中……</Text>
+                <ActivityIndicator />
+                <Text style={styles.loadingTitle}>亲 正在为您加载中 请稍等...</Text>
             </View>
-        )
+        );
     }
 }
 
-
 const styles = StyleSheet.create({
-    loading: {
-        backgroundColor: 'gray',
-        height: 80,
-        width: 100,
-        borderRadius: 10,
-        justifyContent: 'center',
-        alignItems: 'center',
-        position: 'absolute',
-        top: (Common.window.height-80)/2,
-        left: (Common.window.width-100)/2,
+    loading:{
+        backgroundColor:'gray',
+        height:80,
+        width:100,
+        borderRadius:10,
+        justifyContent:'center',
+        alignItems:'center',
+        position:'absolute',
+        top:(Common.window.height - 80) /2,
+        left:(Common.window.width - 100) /2
     },
-
-    loadingTitle: {
-        marginTop: 10,
-        fontSize: 14,
-        color: 'white'
+    loadingTitle:{
+        marginTop:10,
+        fontSize:14,
+        color:'white'
     }
-})
-//todo
+});
