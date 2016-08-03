6d3a5075700e1c7757b87ea21eb9b122dbe96f83
xiaodong <875697126@qq.com>
2016-08-03 16:57:43 +0800

    update

---
+++ app/pages/Class.js
@@ -0,0 +1,15 @@
+import React ,{ Component } from 'react';
+import {
+    StyleSheet,
+    Text,
+    Image,
+    ListView,
+    TouchableOpacity,
+    View,
+    InteractionManager
+} from 'react-native';
+
+import { ClassActin } from '../actions/ClassificationAction';
+import Common from '../common/common';
+import Loading from '../common/Loading';
+import Detail from '../containers/'
